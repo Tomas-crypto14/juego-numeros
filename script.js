5f135a9500357edc276998c894d4a6722ae1e6d8
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-03-28 11:54:51 +0100

    first commit

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let attempts;
 const MAX_ATTEMPTS = 10;
 let max_number;
 const MIN_NUMBER = 1;
-const record = localStorage.getItem("record") || Infinity;
+const record = localStorage.getItem("record") || null;
 // --- Funciones ---
 
 // Función para iniciar o reiniciar el juego
